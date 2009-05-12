f0e5f590eeac5ade05da0471362bcc4c9bded304
David Carroll <david@bvcms.com>
2009-05-12 01:56:34 +0000

    schema update

--- CmsSchema/Tables/dbo.Zips.sql
+++ CmsSchema/Tables/dbo.Zips.sql
@@ -3,6 +3,8 @@ CREATE TABLE [dbo].[Zips]
 [ZipCode] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [MetroMarginalCode] [int] NULL
 )
+ALTER TABLE [dbo].[Zips] ADD
+CONSTRAINT [FK_Zips_ResidentCode] FOREIGN KEY ([MetroMarginalCode]) REFERENCES [lookup].[ResidentCode] ([Id])
 GO
 ALTER TABLE [dbo].[Zips] ADD CONSTRAINT [PK_Zips] PRIMARY KEY CLUSTERED ([ZipCode])
 GO
