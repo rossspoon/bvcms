bcc7bec02e3f437f6dbf8a0396483388c87dca72
Steven Yarbrough <steven@bvcms.com>
2012-07-27 14:56:55 -0500

    Update for JoyRide and tweaks for QuickBooks

---
+++ CmsWeb/Content/js/tutorial.js
@@ -0,0 +1,53 @@
+﻿function setCookie(name, value, seconds)
+{
+	if (seconds) {
+		var date = new Date();
+		date.setTime(date.getTime() + (seconds * 1000));
+		var expires = "; expires=" + date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name)
+{
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+var activeTutorial = "";
+
+function startTutorial(id) {
+	$(".joyride-tip-guide").remove();
+	if ($(id).attr("continues") == "yes") {
+		activeTutorial = id;
+		$(this).joyride({ tipContent: id, postRideCallback: queueNextTutorial, postCancelCallback: clearTutorial });
+	}
+	else {
+		$(this).joyride({ tipContent: id, postRideCallback: clearTutorial, postCancelCallback: clearTutorial });
+	}
+}
+
+function queueNextTutorial() {
+	setCookie("tutorialActive", activeTutorial, 30);
+}
+
+function resumeTutorial() {
+	var id = getCookie("tutorialActive");
+	if (id == null) return;
+	startTutorial(id);
+}
+
+function clearTutorial() {
+	setCookie("tutorialActive", "", -120);
+}
+
+$(document).ready(function () {
+	resumeTutorial();
+})
--- CmsWeb/Scripts/headermenu.js
+++ CmsWeb/Scripts/headermenu.js
@@ -1,63 +1,62 @@
 ﻿
 $(document).ready(function () {
-    $('#SearchText').keypress(function (e) {
-        if ((e.keyCode || e.which) == 13) {
-            e.preventDefault();
-            var s = $('#SearchText').val();
-            if (s != "") {
-                s = s.replace(/^\s+/g, "");
-                s = s.replace(/\s+$/g, " ");
-                var u = '/QuickSearch?name=' + escape(s);
-                window.location = u;
-            }
-        }
-        return true;
-    });
-    $("a.tutorial").click(function (ev) {
-        ev.preventDefault();
-        var a = $(this);
-        $(".joyride-tip-guide").remove();
-        $(this).joyride({ tipContent: a.attr("href") });
-    });
-    $('#AddDialog').dialog({
-        bgiframe: true,
-        autoOpen: false,
-        width: 750,
-        height: 700,
-        modal: true,
-        overlay: {
-            opacity: 0.5,
-            background: "black"
-        }, close: function () {
-            $('iframe', this).attr("src", "");
-        }
-    });
-    $('#addpeople').click(function (e) {
-        e.preventDefault();
-        var d = $('#AddDialog');
-        $('iframe', d).attr("src", "/SearchAdd?type=addpeople");
-        d.dialog("option", "title", "Add People");
-        d.dialog("open");
-    });
-    $('#addorg').click(function (e) {
-        e.preventDefault();
-        var d = $('#AddDialog');
-        $('iframe', d).attr("src", "/AddOrganization");
-        d.dialog("option", "title", "Add Organization");
-        d.dialog("open");
-    });
-    $('#cleartag').click(function (e) {
-        e.preventDefault();
-        //if (alert("are you sure?"))
-        $.post("/Tags/ClearTag", {}, function () {
-            window.location.reload();
-        });
-    });
-    $('.warntip').tooltip({
-        delay: 150,
-        showBody: "|",
-        showURL: false
-    });
+	$('#SearchText').keypress(function (e) {
+		if ((e.keyCode || e.which) == 13) {
+			e.preventDefault();
+			var s = $('#SearchText').val();
+			if (s != "") {
+				s = s.replace(/^\s+/g, "");
+				s = s.replace(/\s+$/g, " ");
+				var u = '/QuickSearch?name=' + escape(s);
+				window.location = u;
+			}
+		}
+		return true;
+	});
+	$("a.tutorial").click(function (ev) {
+		ev.preventDefault();
+		alert($(this).attr("href"));
+		startTutorial($(this).attr("href"));
+	});
+	$('#AddDialog').dialog({
+		bgiframe: true,
+		autoOpen: false,
+		width: 750,
+		height: 700,
+		modal: true,
+		overlay: {
+			opacity: 0.5,
+			background: "black"
+		}, close: function () {
+			$('iframe', this).attr("src", "");
+		}
+	});
+	$('#addpeople').click(function (e) {
+		e.preventDefault();
+		var d = $('#AddDialog');
+		$('iframe', d).attr("src", "/SearchAdd?type=addpeople");
+		d.dialog("option", "title", "Add People");
+		d.dialog("open");
+	});
+	$('#addorg').click(function (e) {
+		e.preventDefault();
+		var d = $('#AddDialog');
+		$('iframe', d).attr("src", "/AddOrganization");
+		d.dialog("option", "title", "Add Organization");
+		d.dialog("open");
+	});
+	$('#cleartag').click(function (e) {
+		e.preventDefault();
+		//if (alert("are you sure?"))
+		$.post("/Tags/ClearTag", {}, function () {
+			window.location.reload();
+		});
+	});
+	$('.warntip').tooltip({
+		delay: 150,
+		showBody: "|",
+		showURL: false
+	});
 });
 function CloseAddDialog() {
     $("#AddDialog").dialog("close");
