2a7f052090a4ae28a6e6f74aaac70d8657736a58
Steven Yarbrough <steven@bvcms.com>
2012-09-25 11:12:32 -0500

    Update to JSON Helper to handle dates properly

--- UtilityExtensions/JSONHelper.cs
+++ UtilityExtensions/JSONHelper.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace UtilityExtensions
 {
@@ -19,15 +20,46 @@ namespace UtilityExtensions
 			ser.WriteObject(ms, t);
 			string jsonString = Encoding.UTF8.GetString(ms.ToArray());
 			ms.Close();
+
+			string p = @"\\/Date\((\d+)[\+-]\d+\)\\/";
+			MatchEvaluator matchEvaluator = new MatchEvaluator(ConvertJsonDateToDateString);
+			Regex reg = new Regex(p);
+			jsonString = reg.Replace(jsonString, matchEvaluator);
+
 			return jsonString;
 		}
 
 		public static T JsonDeserialize<T>(string jsonString)
 		{
+			string p = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}";
+			MatchEvaluator matchEvaluator = new MatchEvaluator(ConvertDateStringToJsonDate);
+			Regex reg = new Regex(p);
+			jsonString = reg.Replace(jsonString, matchEvaluator);
+
 			DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
 			MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
 			T obj = (T)ser.ReadObject(ms);
 			return obj;
+		}
+
+		private static string ConvertJsonDateToDateString(Match m)
+		{
+			string result = string.Empty;
+			DateTime dt = new DateTime(1970, 1, 1);
+			dt = dt.AddMilliseconds(long.Parse(m.Groups[1].Value));
+			dt = dt.ToLocalTime();
+			result = dt.ToString("yyyy-MM-dd HH:mm:ss");
+			return result;
+		}
+
+		private static string ConvertDateStringToJsonDate(Match m)
+		{
+			string result = string.Empty;
+			DateTime dt = DateTime.Parse(m.Groups[0].Value);
+			dt = dt.ToUniversalTime();
+			TimeSpan ts = dt - DateTime.Parse("1970-01-01");
+			result = string.Format("\\/Date({0}+0800)\\/", ts.TotalMilliseconds);
+			return result;
 		}
 	}
 }
