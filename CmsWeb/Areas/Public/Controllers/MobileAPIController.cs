be99a439dea518baccc28b6db58e52b105414e21
Steven Yarbrough <steven@bvcms.com>
2012-09-25 11:47:12 -0500

    Start of the MobileAPI

--- CmsWeb/Areas/Public/Controllers/MobileAPIController.cs
+++ CmsWeb/Areas/Public/Controllers/MobileAPIController.cs
@@ -27,20 +27,131 @@ namespace CmsWeb.Areas.Public.Controllers
 
 		public ActionResult Search(string name, string comm, string addr)
 		{
-			List<MobilePerson> mp = new List<MobilePerson>();
 			BaseReturn br = new BaseReturn();
+			List<MobilePerson> mp = new List<MobilePerson>();
 
 			var m = new SearchModel(name, comm, addr);
 
 			br.type = 1;
 			br.count = m.Count;
 
-			foreach (var item in m.ApplySearch().OrderBy(p => p.Name2).Take(20))
+			foreach( var item in m.ApplySearch().OrderBy(p => p.Name2).Take(20) )
 			{
 				mp.Add(new MobilePerson().populate(item));
 			}
 
 			br.data = JSONHelper.JsonSerializer<List<MobilePerson>>(mp);
+			return br;
+		}
+
+		public ActionResult TaskList( int ID )
+		{
+			BaseReturn br = new BaseReturn();
+			List<MobileTask> mt = new List<MobileTask>();
+
+			var list = from e in DbUtil.Db.Tasks
+					   where e.OwnerId == ID
+					   select e;
+
+			br.type = 101;
+
+			if (list != null)
+			{
+				br.count = list.Count();
+
+				foreach (var item in list)
+				{
+					mt.Add(new MobileTask().populate(item));
+				}
+
+				br.data = JSONHelper.JsonSerializer<List<MobileTask>>(mt);
+			}
+			else
+			{
+				br.error = 1;
+			}
+
+			return br;
+		}
+
+		public ActionResult TaskItem( int ID )
+		{
+			BaseReturn br = new BaseReturn();
+			MobileTask mt = new MobileTask();
+
+			var item = (from e in DbUtil.Db.Tasks
+					   where e.Id == ID
+					   select e).SingleOrDefault();
+
+			br.type = 102;
+
+			if (item != null)
+			{
+				br.count = 1;
+
+				mt.populate(item);
+				br.data = JSONHelper.JsonSerializer<MobileTask>(mt);
+			}
+			else
+			{
+				br.error = 1;
+			}
+			
+			return br;
+		}
+
+		public ActionResult TaskBoxList( int ID )
+		{
+			BaseReturn br = new BaseReturn();
+			List<MobileTaskBox> mtb = new List<MobileTaskBox>();
+
+			var list = from e in DbUtil.Db.TaskLists
+					   where e.CreatedBy == ID
+					   select e;
+
+			br.type = 103;
+
+			if (list != null)
+			{
+				br.count = list.Count();
+
+				foreach (var item in list)
+				{
+					mtb.Add(new MobileTaskBox().populate(item));
+				}
+
+				br.data = JSONHelper.JsonSerializer<List<MobileTaskBox>>(mtb);
+			}
+			else
+			{
+				br.error = 1;
+			}
+
+			return br;
+		}
+
+		public ActionResult TaskBoxItem( int ID )
+		{
+			BaseReturn br = new BaseReturn();
+			MobileTaskBox mtb = new MobileTaskBox();
+
+			var item = (from e in DbUtil.Db.TaskLists
+					   where e.CreatedBy == ID
+					   select e).SingleOrDefault();
+
+			br.type = 104;
+
+			if (item != null)
+			{
+				br.count = 1;
+
+				mtb.populate(item);
+				br.data = JSONHelper.JsonSerializer<MobileTaskBox>(mtb);
+			}
+			else
+			{
+				br.error = 1;
+			}
 
 			return br;
 		}
---
+++ CmsWeb/Areas/Public/Models/MobileAPI/MobileTask.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsWeb.MobileAPI
+{
+	/*
+	+ [Id] [int] IDENTITY(1,1) NOT NULL,
+	[OwnerId] [int] NOT NULL,
+	[ListId] [int] NOT NULL,
+	+ [CoOwnerId] [int] NULL,
+	[CoListId] [int] NULL,
+	+ [StatusId] [int] NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[SourceContactId] [int] NULL,
+	[CompletedContactId] [int] NULL,
+	+ [Notes] [varchar](max) NULL,
+	[ModifiedBy] [int] NULL,
+	[ModifiedOn] [datetime] NULL,
+	[Project] [varchar](50) NULL,
+	[Archive] [bit] NOT NULL,
+	+ [Priority] [int] NULL,
+	+ [WhoId] [int] NULL,
+	+ [Due] [datetime] NULL,
+	[Location] [varchar](50) NULL,
+	+ [Description] [varchar](100) NULL,
+	[CompletedOn] [datetime] NULL,
+	[ForceCompleteWContact] [bit] NULL,
+	[OrginatorId] [int] NULL,
+	*/
+
+	public class MobileTask
+	{
+		public int id = 0;
+
+		public DateTime due = new DateTime();
+		public int priority = 0;
+		
+		public string description = "";
+
+		public string status = "";
+		public int statusID = 0;
+
+		public string about = "";
+		public int aboutID = 0;
+
+		public string delegated = "";
+		public int delegatedID = 0;
+
+		public string notes = "";
+
+		public MobileTask populate(CmsData.Task t)
+		{
+			id = t.Id;
+
+			due = t.Due ?? DateTime.Now;
+			priority = t.Priority ?? 0;
+
+			description = t.Description ?? "";
+
+			status = t.TaskStatus.Description ?? "";
+			statusID = t.StatusId ?? 0;
+
+			about = t.AboutName ?? "";
+			aboutID = t.WhoId ?? 0;
+
+			if( t.CoOwner != null ) delegated = t.CoOwner.Name ?? "";
+			delegatedID = t.CoOwnerId ?? 0;
+
+			notes = t.Notes ?? "";
+
+			return this;
+		}
+	}
+}
