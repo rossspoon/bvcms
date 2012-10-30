7c2ee09da6a0b59ec8c142ea79e7a379695f5bb6
Steven Yarbrough <steven@bvcms.com>
2012-10-30 09:19:40 -0500

    Update for e-Mail Templates - Changed setting to do local or remote thumbnails - Added thumbnail display in Special Content

--- CmsWeb/Areas/Manage/Controllers/DisplayController.cs
+++ CmsWeb/Areas/Manage/Controllers/DisplayController.cs
@@ -16,6 +16,12 @@ using CmsWeb.Models;
 using System.Data.SqlClient;
 // Used for pulling image from service
 using System.Net;
+// Used for HTML Image Capture
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace CmsWeb.Areas.Manage.Controllers
 {
@@ -68,19 +74,30 @@ namespace CmsWeb.Areas.Manage.Controllers
 			content.Body = body;
 			content.RoleID = roleid ?? 0;
 
-            if (DbUtil.Db.Setting("RenderEmailTemplate", "false") == "true")
+            string sRenderType = DbUtil.Db.Setting("RenderEmailTemplate", "none");
+
+            switch (sRenderType)
             {
-                var coll = new NameValueCollection();
-			    coll.Add("sHTML", body.Replace("\r", "").Replace("\n","").Replace("\t","") );
+                case "Local": // Uses local server resources
+                {
+                    if (content.ThumbID != 0) ImageData.Image.UpdateImageFromBits(content.ThumbID, CaptureWebPageBytes(body, 100, 150));
+                    else content.ThumbID = ImageData.Image.NewImageFromBits(CaptureWebPageBytes(body, 100, 150)).Id;
+                    break;
+                }
 
-                var wc = new WebClient();
-                var resp = wc.UploadValues("http://localhost:8887/Home/CreateWebsiteThumbnail", "POST", coll);
+                case "Service": // Used to send HTML to another server for offloaded processing
+                {
+                    var coll = new NameValueCollection();
+                    coll.Add("sHTML", body.Replace("\r", "").Replace("\n", "").Replace("\t", ""));
 
-                if (content.ThumbID != 0) ImageData.Image.UpdateImageFromBits(content.ThumbID, resp);
-                else content.ThumbID = ImageData.Image.NewImageFromBits(resp).Id;
-                
-                //if (content.ThumbID != 0) ImageData.Image.UpdateImageFromBits(content.ThumbID, CaptureWebPageBytes(body, 100, 150));
-                //else content.ThumbID = ImageData.Image.NewImageFromBits(CaptureWebPageBytes(body, 100, 150)).Id;
+                    var wc = new WebClient();
+                    var resp = wc.UploadValues("http://localhost:8887/Home/CreateWebsiteThumbnail", "POST", coll);
+
+                    if (content.ThumbID != 0) ImageData.Image.UpdateImageFromBits(content.ThumbID, resp);
+                    else content.ThumbID = ImageData.Image.NewImageFromBits(resp).Id;
+
+                    break;
+                }
             }
 
 			DbUtil.Db.SubmitChanges();
@@ -169,5 +186,119 @@ namespace CmsWeb.Areas.Manage.Controllers
 			DbUtil.Db.SubmitChanges();
 			return Redirect("/Organization/Index/" + id);
 		}
+
+        public static byte[] CaptureWebPageBytes(string body, int width, int height)
+        {
+            bool bDone = false;
+            byte[] data = null;
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now;
+
+            //sta thread to allow intiate WebBrowser
+            var staThread = new Thread(delegate()
+            {
+                data = CaptureWebPageBytesP(body, width, height);
+                bDone = true;
+            });
+
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+
+            while (!bDone)
+            {
+                endDate = DateTime.Now;
+                TimeSpan tsp = endDate.Subtract(startDate);
+
+                System.Windows.Forms.Application.DoEvents();
+                if (tsp.Seconds > 50)
+                {
+                    break;
+                }
+            }
+            staThread.Abort();
+            return data;
+        }
+
+        static byte[] CaptureWebPageBytesP(string body, int width, int height)
+        {
+            byte[] data;
+
+            using (WebBrowser web = new WebBrowser())
+            {
+                web.ScrollBarsEnabled = false; // no scrollbars
+                web.ScriptErrorsSuppressed = true; // no errors
+
+                web.DocumentText = body;
+                while (web.ReadyState != System.Windows.Forms.WebBrowserReadyState.Complete)
+                    System.Windows.Forms.Application.DoEvents();
+
+                web.Width = web.Document.Body.ScrollRectangle.Width;
+                web.Height = web.Document.Body.ScrollRectangle.Height;
+
+                // a bitmap that we will draw to
+                using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(web.Width, web.Height))
+                {
+                    // draw the web browser to the bitmap
+                    web.DrawToBitmap(bmp, new Rectangle(web.Location.X, web.Location.Y, web.Width, web.Height));
+                    // draw the web browser to the bitmap
+
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                    RectangleF destRect = new RectangleF(0F, 0F, width, height);
+                    RectangleF srcRect = new RectangleF(0, 0, web.Width, web.Width * 1.5F);
+
+                    Bitmap b = new Bitmap(width, height);
+                    Graphics g = Graphics.FromImage((Image)b);
+                    g.Clear(Color.White);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                    g.DrawImage(bmp, destRect, srcRect, units);
+                    g.Dispose();
+
+                    using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
+                    {
+                        EncoderParameter qualityParam = null;
+                        EncoderParameters encoderParams = null;
+                        try
+                        {
+                            ImageCodecInfo imageCodec = null;
+                            imageCodec = GetEncoderInfo("image/jpeg");
+
+                            qualityParam = new EncoderParameter(Encoder.Quality, 100L);
+
+                            encoderParams = new EncoderParameters(1);
+                            encoderParams.Param[0] = qualityParam;
+                            b.Save(stream, imageCodec, encoderParams);
+                        }
+                        catch (Exception)
+                        {
+                            throw new Exception();
+                        }
+                        finally
+                        {
+                            if (encoderParams != null) encoderParams.Dispose();
+                            if (qualityParam != null) qualityParam.Dispose();
+                        }
+                        b.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        stream.Position = 0;
+                        data = new byte[stream.Length];
+                        stream.Read(data, 0, (int)stream.Length);
+                    }
+                }
+            }
+            return data;
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
 	}
 }
