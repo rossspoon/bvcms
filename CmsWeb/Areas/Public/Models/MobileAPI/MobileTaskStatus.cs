f7953ce9226b56385db801522f25afce7e9ce58b
Steven Yarbrough <steven@bvcms.com>
2012-11-27 14:53:15 -0600

    Update to MobileAPI

---
+++ CmsWeb/Areas/Public/Models/MobileAPI/MobileTaskStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CmsData;
+using UtilityExtensions;
+
+namespace CmsWeb.MobileAPI
+{
+    public class MobileTaskStatus
+    {
+        public int id = 0;
+        public string code = "";
+        public string description = "";
+
+        public MobileTaskStatus populate(CmsData.TaskStatus t)
+        {
+            id = t.Id;
+            code = t.Code;
+            description = t.Description;
+
+            return this;
+        }
+    }
+}
