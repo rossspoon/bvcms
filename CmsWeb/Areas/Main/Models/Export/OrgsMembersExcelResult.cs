31c2f8219d72fd1858a13e10f5362905ea4fb51e
David Carroll <david@bvcms.com>
2013-07-03 13:32:03 -0500

    OrgsMembersExcel export from OrgSearch

---
+++ CmsWeb/Areas/Main/Models/Export/OrgsMembersExcelResult.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using UtilityExtensions;
+using System.Linq;
+using CmsData;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace CmsWeb.Models
+{
+    public class OrgsMembersExcelResult : ActionResult
+    {
+        private OrgSearchModel m;
+        public OrgsMembersExcelResult(OrgSearchModel m)
+        {
+            this.m = m;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var Response = context.HttpContext.Response;
+            Response.Buffer = true;
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", "attachment;filename=CMSOrganizations.xls");
+            Response.Charset = "";
+            var d = m.OrgsMemberList();
+            var dg = new DataGrid();
+            dg.DataSource = d;
+            dg.DataBind();
+            dg.RenderControl(new HtmlTextWriter(Response.Output));
+        }
+    }
+}
