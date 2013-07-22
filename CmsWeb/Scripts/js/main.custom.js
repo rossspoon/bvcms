dd3ad514a26b47828a8caa75f42f981e5e29ad48
David Carroll <david@bvcms.com>
2013-07-22 15:30:58 -0500

    Changes to support

--- CmsWeb/Scripts/js/main.custom.js
+++ CmsWeb/Scripts/js/main.custom.js
@@ -526,19 +526,20 @@ function CloseAddOrgDialog(id) {
 var mustSearch = 1;
 
 $(function () {
-    $(document).on("click", "#supportfeedback", null, supportOpen);
-    $(document).on("keyup", "#gsc-i-id1", null, scheduleRefresh);
+    $(document).on("click", "#supportfeedback,#emailsupport", null, supportOpen);
+    $(document).on("keyup", "#gsc-i-id1", null, searchRequested);
     $(document).on("click", ".gsc-search-button", null, searchDone);
+    $(document).on("click", "#showSupportForm", null, supportShowForm);
     $(document).on("click", "#sendSupport", null, supportSend);
     $(document).on("click", "#cancelSupport", null, supportClose);
     $(document).on("click", "[supportHelp]", null, supportHelpOpen);
     $(document).on("click", "[supportCancelHelp]", null, supportHelpClose);
 
     $("#supportDialog").dialog({ autoOpen: false, resizable: false, width: 800, height: 500, modal: true, dialogClass: "no-title" });
-    $("#supportHelp").dialog({ autoOpen: false, resizable: false, width: "auto", height: "auto", modal: true, dialogClass: "no-title" });
+    $("#supportHelp").dialog({ autoOpen: false, resizable: false, width: 500, height: "auto", modal: true, dialogClass: "no-title" });
 });
 
-function scheduleRefresh(e) {
+function searchRequested(e) {
     if (e.keyCode == 13)
         searchDone(e);
 }
@@ -546,19 +547,23 @@ function scheduleRefresh(e) {
 function searchDone(e) {
     mustSearch--;
     if (mustSearch <= 0) {
-        $("#reqBody").removeAttr("disabled");
-        $("#sendSupport").removeAttr("disabled");
-        $("#reqSelect").removeAttr("disabled");
+        $("#showSupportForm").button( "option", "disabled", false );
     }
+}
 
+function supportShowForm(e) {
+    $("#reqBody").val("");
+
+    $("#supportInstructions").css("display", "none");
+    $("#supportForm").css("display", "block");
 }
 
 function supportOpen(e) {
     mustSearch = 1;
 
-    $("#reqBody").attr("disabled", "disabled").val("");
-    $("#sendSupport").attr("disabled", "disabled");
-    $("#reqSelect").attr("disabled", "disabled");
+    $("#showSupportForm").button("option", "disabled", true);
+    $("#supportInstructions").css("display", "block");
+    $("#supportForm").css("display", "none");
 
     $("#supportDialog").dialog("open");
 }
@@ -568,13 +573,21 @@ function supportClose(e) {
 }
 
 function supportSend(e) {
+    $("#supportLastSearch").val($("#gsc-i-id1").val());
+
     var theURL = "/Home/SendSupportRequest";
     var thePost = $(this).closest("form").serialize();
     $.ajax(theURL, { data: thePost, type: "POST", success: supportSentComplete });
 }
 
 function supportSentComplete(data, status, xhr) {
-    $("#supportDialog").dialog("close");
+    if (data == "OK") {
+        alert("Your request has been submitted!");
+        $("#supportDialog").dialog("close");
+    }
+    else {
+        alert("There was an error submitting your support request, please try again.");
+    }
 }
 
 function supportHelpOpen(e) {
