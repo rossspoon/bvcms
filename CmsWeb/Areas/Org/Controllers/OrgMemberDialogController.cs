2b366edd2c29bfef87e161269adb7d1f07e43b50
David Carroll <david@bvcms.com>
2013-07-22 07:16:54 -0500

    Working on New UI

--- CmsWeb/Areas/Org/Controllers/OrgMemberDialogController.cs
+++ CmsWeb/Areas/Org/Controllers/OrgMemberDialogController.cs
@@ -23,18 +23,17 @@ namespace CmsWeb.Areas.Org.Controllers
             var m = new OrgMemberModel {OrgId = oid, PeopleId = pid};
             return View(m);
         }
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult CheckBoxChanged(string id, bool ck)
+        [POST("OrgMemberDialog2/SmallGroupChecked/{oid:int}/{pid:int}/{sgtagid:int}")]
+        public ActionResult SmallGroupChecked(int oid, int pid, int sgtagid, bool ck)
         {
-            var a = id.Split('-');
-            var om = DbUtil.Db.OrganizationMembers.SingleOrDefault(m => m.PeopleId == a[2].ToInt() && m.OrganizationId == a[1].ToInt());
+            var om = DbUtil.Db.OrganizationMembers.SingleOrDefault(m => m.PeopleId == pid && m.OrganizationId == oid);
             if (om == null)
                 return Content("error");
             if (ck)
-                om.OrgMemMemTags.Add(new OrgMemMemTag { MemberTagId = a[3].ToInt() });
+                om.OrgMemMemTags.Add(new OrgMemMemTag { MemberTagId = sgtagid });
             else
             {
-                var mt = om.OrgMemMemTags.SingleOrDefault(t => t.MemberTagId == a[3].ToInt());
+                var mt = om.OrgMemMemTags.SingleOrDefault(t => t.MemberTagId == sgtagid);
 				if (mt == null)
 					return Content("not found");
                 DbUtil.Db.OrgMemMemTags.DeleteOnSubmit(mt);
@@ -62,11 +61,10 @@ namespace CmsWeb.Areas.Org.Controllers
             }
             return View("Display", m);
         }
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Drop(string id)
+        [POST("OrgMemberDialog2/Drop/{oid:int}/{pid:int}")]
+        public ActionResult Drop(int oid, int pid)
         {
-            var a = id.Split('-');
-            var om = DbUtil.Db.OrganizationMembers.SingleOrDefault(m => m.PeopleId == a[2].ToInt() && m.OrganizationId == a[1].ToInt());
+            var om = DbUtil.Db.OrganizationMembers.SingleOrDefault(m => m.PeopleId == pid && m.OrganizationId == oid);
             if (om != null)
             {
                 om.Drop(DbUtil.Db, addToHistory:true);
@@ -74,39 +72,25 @@ namespace CmsWeb.Areas.Org.Controllers
             }
             return Content("dropped");
         }
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Move(int id, int pid)
+        [POST("OrgMemberDialog2/Move/{oid:int}/{pid:int}")]
+        public ActionResult Move(int oid, int pid)
         {
-            var om = DbUtil.Db.OrganizationMembers.Single(m => m.PeopleId == pid && m.OrganizationId == id);
-            ViewData["name"] = om.Person.Name;
-            ViewData["oid"] = id;
-            ViewData["pid"] = pid;
-            if (om.Organization.DivisionId == null)
-                return View((IEnumerable<OrgMove>)null);
-			var divorgs = om.Organization.DivOrgs.Select(mm => mm.DivId).ToList();
-            var q = from o in DbUtil.Db.Organizations
-                    where o.DivOrgs.Any(dd => divorgs.Contains(dd.DivId))
-                    where o.OrganizationId != id
-                    where o.OrganizationStatusId == OrgStatusCode.Active
-                    orderby o.OrganizationName
-                    select new OrgMove
-                    {
-                         OrgName = o.OrganizationName,
-                         OrgId = o.OrganizationId,
-                         id = "m-{0}-{1}-{2}".Fmt(id, pid, o.OrganizationId),
-                         Program = o.Division.Program.Name,
-                         Division = o.Division.Name,
-                         orgSchedule = o.OrgSchedules.First()
-                    };
-            return View(q.ToList());
+            var mm = new OrgMemberMoveModel {OrgId = oid, PeopleId = pid};
+            mm.Pager.SetWithPageOnly("/OrgMemberDialog2/MoveResults", 1);
+            return View(mm);
         }
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult MoveSelect(string id)
+        [POST("OrgMemberDialog2/MoveResults/{page}")]
+        public ActionResult MoveResults(int page, OrgMemberMoveModel m)
         {
-            var a = id.Split('-');
-            var om1 = DbUtil.Db.OrganizationMembers.Single(m => m.PeopleId == a[2].ToInt() && m.OrganizationId == a[1].ToInt());
+            m.Pager.SetWithPageOnly("/OrgMemberDialog2/MoveResults", page);
+            return View("Move", m);
+        }
+        [POST("OrgMemberDialog2/MoveSelect/{oid:int}/{pid:int}/{toid:int}")]
+        public ActionResult MoveSelect(int oid, int pid, int toid)
+        {
+            var om1 = DbUtil.Db.OrganizationMembers.Single(m => m.PeopleId == pid && m.OrganizationId == oid);
             var om2 = CmsData.OrganizationMember.InsertOrgMembers(DbUtil.Db,
-                a[3].ToInt(), om1.PeopleId, om1.MemberTypeId, DateTime.Now, om1.InactiveDate, om1.Pending ?? false);
+                toid, om1.PeopleId, om1.MemberTypeId, DateTime.Now, om1.InactiveDate, om1.Pending ?? false);
 			DbUtil.Db.UpdateMainFellowship(om2.OrganizationId);
 			om2.EnrollmentDate = om1.EnrollmentDate;
 			if (om2.EnrollmentDate.Value.Date == DateTime.Today)
@@ -117,23 +101,6 @@ namespace CmsWeb.Areas.Org.Controllers
             om1.Drop(DbUtil.Db, addToHistory:true);
             DbUtil.Db.SubmitChanges();
             return Content("moved");
-        }
-        public class OrgMove
-        {
-            public string OrgName { get; set; }
-            public string id { get; set; }
-            public int OrgId { get; set; }
-            public string Program { get; set; }
-            public string Division { get; set; }
-            public OrgSchedule orgSchedule { get; set; }
-            public string Tip
-            {
-                get
-                {
-                    var si = new ScheduleInfo(orgSchedule);
-                    return "{0} ({1})|Program:{2}|Division: {3}|Schedule: {4}".Fmt(OrgName, OrgId, Program, Division, si.Display);
-                }
-            }
         }
         public string HelpLink()
         {
